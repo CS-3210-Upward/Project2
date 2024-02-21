88b2165bf6ad53254c1d257ef472e82eb4577d5a
MattRMorgan <69861749+MattRMorgan@users.noreply.github.com>
2024-02-21 15:30:12 -0700

    base code

---
+++ project2.py
@@ -0,0 +1,19 @@
+file_name = input("please enter the name of the file: ")
+count = 0
+prj_file = open(file_name, "r")
+
+content = prj_file.read()
+
+for line in content:
+
+
+def check_indentation(file):
+	return(new_file)
+
+def check_headers(file):
+	return (new_file)
+
+def count_prints(file):
+	return(new_file)
+
+print(count)
