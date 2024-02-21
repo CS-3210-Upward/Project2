7325b65cc4cd67c0ede10f0ab46da7aed05711f6
dhelms33 <derrickhelms10@gmail.com>
2024-02-21 15:31:19 -0700

    Added Lexical Analyzer Python code

--- Lexical_Analyzer.py
+++ Lexical_Analyzer.py
@@ -6,24 +6,24 @@ class LexicalAnalyzer:
     
     def check_identation( file):
         """This function checks for correct indentation"""
-        with open(file, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                if line.strip() != "":
-                    if len(line) % 4 != 0:
-                        line = "    " + line
-        with open("updated_file.txt", "w") as f:
-            f.writelines(lines)
-        return "updated_file.txt"
+        with open(file, 'r') as f: #open the file and read the lines
+            lines = f.readlines() 
+            for line in lines: #iterate through the lines
+                if line.strip() != "": #strip the line of any white space
+                    if len(line) % 4 != 0: #if the number of spaces is not a multiple of 4, or a tab
+                        line = "    " + line #fix the indentation
+        with open("updated_file.txt", "w") as f: #write the updated lines to a new file
+            f.writelines(lines) 
+        return "updated_file.txt" #return the name of the new file
     
-    def verify_function_headers(file):
+    def verify_function_headers(file): 
         """This function verifies the syntactic correctness of all function headers"""
-        with open(file, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                if "def" in line:
-                    if line[-1] != ":":
-                        line = line[:-1] + ":"
+        with open(file, 'r') as f: #open the file and read the lines
+            lines = f.readlines() 
+            for line in lines: #iterate through the lines
+                if "def" in line: #if the line is a function header
+                    if line[-1] != ":": #if the function header is not correct
+                        line = line[:-1] + ":" #remove last character
         with open("updated_file.txt", "w") as f:
             f.writelines(lines)
         return "updated_file.txt"
