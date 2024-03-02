f8d82378c489e296ed2fc47889a0f1ca1e95d779
dhelms33 <derrickhelms10@gmail.com>
2024-03-01 20:53:43 -0700

    Attempted to fix lexcial analyzer

--- Lexical_Analyzer.py
+++ Lexical_Analyzer.py
@@ -16,40 +16,44 @@ class LexicalAnalyzer:
             f.writelines(lines)
         return "updated_file.txt"
 
-    def verify_function_headers(self):
-        """This function verifies the syntactic correctness of all function headers"""
-        with open(self.file, 'r') as f:
-            lines = f.readlines()
-            for i, line in enumerate(lines):
-                if "def" in line:
-                    # Check for parentheses
-                    if "(" in line and ")" in line:
-                        # Split the line into function name and arguments
-                        func_name, args_part = line.split("(", 1)
-                        
-                        # Check if there are multiple arguments
-                        if "," in args_part:
-                            # Split arguments and add commas if needed
-                            args = args_part.split(",")
-                            args = [arg.strip() for arg in args]
-                            args_part = ", ".join(args)
-                        
-                        # Update the line with parentheses and colon
-                        lines[i] = f"{func_name}({args_part[:-1]}):\n"
-                    elif "(" in line:
-                        # Handle the case where there is an open parenthesis but no closing parenthesis
-                        lines[i] = line.rstrip() + ")"
-                    elif ")" in line:
-                        # Handle the case where there is a closing parenthesis but no open parenthesis
-                        lines[i] = "(" + line.lstrip()
-                    else:
-                        # If there are no parentheses, just add parentheses and colon
-                        lines[i] = line.rstrip() + "():"
-        
-        with open("updated_file.txt", "w") as f:
-            f.writelines(lines)
-        
-        return "updated_file.txt"
+def verify_function_headers(self):
+    """This function verifies the syntactic correctness of all function headers"""
+    with open(self.file, 'r') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if "def" in line:
+                # Check for parentheses
+                if "(" in line and ")" in line:
+                    # Split the line into function name and arguments
+                    func_name, args_part = line.split("(", 1)
+
+                    # Check if there are multiple arguments
+                    if "," in args_part:
+                        # Split arguments and add commas if needed
+                        args = args_part.split(",")
+                        args = [arg.strip() for arg in args]
+                        args_part = ", ".join(args)
+
+                    # Update the line with parentheses and colon
+                    lines[i] = f"{func_name}({args_part[:-1]}):\n"
+                elif "(" in line:
+                    # Handle the case where there is an open parenthesis but no closing parenthesis
+                    lines[i] = line.rstrip() + ")"
+                elif ")" in line:
+                    # Handle the case where there is a closing parenthesis but no open parenthesis
+                    lines[i] = "(" + line.lstrip()
+                else:
+                    # If there are no parentheses, just add parentheses and colon
+                    lines[i] = line.rstrip() + "():"
+            else:
+                # If "def" is not found, replace the line with a default function header
+                lines[i] = "def default_function():\n"
+
+    with open("updated_file.txt", "w") as f:
+        f.writelines(lines)
+
+    return "updated_file.txt"
+
 
     def count_print(self):
         """This function counts the occurrences of the keyword 'print' """
--- testfile2.py
+++ testfile2.py
@@ -1,6 +1,6 @@
 def example_function1   (parameter1)   
     print("Inside example_function1")
     if parameter1:
-      print("Parameter is True")
+    print("Parameter is True")
   else:
-    print("Parameter is False")
+        print("Parameter is False")
